0a80e8ce7e9b7c4c6b12b5a50f1d8f6207e145b5
Emilian Roman <git@miris.design>
2023-08-18 21:27:39 +0800

    Add rudimentary querying page
    Signed-off-by: Emilian Roman <git@miris.design>

---
+++ app/Http/Controllers/QueryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use JetBrains\PhpStorm\NoReturn;
+
+class QueryController extends Controller
+{
+    #[NoReturn]
+    public function query(Request $request)
+    {
+        $response = json_decode(Http::get('http://localhost:8001', [
+            'query' => $request->input('query'),
+        ])->body());
+
+        return view('welcome', [
+            'query' => $response->query,
+            'results' => $response->result
+        ]);
+    }
+}
