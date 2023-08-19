7994ac384805c87d6945cdfd669ee656c9513d0d
Emilian Roman <git@miris.design>
2023-08-20 00:32:52 +0800

    Handle back-end errors in a somewhat graceful way

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -13,29 +13,35 @@ class QueryController extends Controller
     #[NoReturn]
     public function query(Request $request)
     {
-        $response = json_decode(Http::timeout(0)->get(config('app.api'), [
-            'query'    => $request->input('query'),
-            'demo'     => $request->has('demo') ? 'yes' : 'no',
-            'instance' => config('app.gpt')
-        ])->body());
+        try {
+            $response = json_decode(Http::timeout(0)->get(config('app.api'), [
+                'query'    => $request->input('query'),
+                'demo'     => $request->has('demo') ? 'yes' : 'no',
+                'instance' => config('app.gpt')
+            ])->body());
 
-        $categories = Category::query()->with('keywords.documents');
-        $documents  = Document::query();
+            $categories = Category::query()->with('keywords.documents');
+            $documents  = Document::query();
 
-        foreach ($response->sources as $source) {
-            $documents = $documents->orWhere('name', $source->document);
+            foreach ($response->sources as $source) {
+                $documents = $documents->orWhere('name', $source->document);
 
-            $categories = $categories->orWhereHas('keywords.documents', function ($q) use ($source) {
-                return $q->where('name', $source->document);
-            });
+                $categories = $categories->orWhereHas('keywords.documents', function ($q) use ($source) {
+                    return $q->where('name', $source->document);
+                });
+            }
+
+            return view('results', [
+                'query'      => $response->query,
+                'answer'     => $response->answer,
+                'sources'    => $response->sources,
+                'categories' => $categories->get(),
+                'documents'  => $documents->get(),
+            ]);
+        } catch (\Exception $exception) {
+            return view('error', [
+                'error' => $exception->getMessage()
+            ]);
         }
-
-        return view('results', [
-            'query'      => $response->query,
-            'answer'     => $response->answer,
-            'sources'    => $response->sources,
-            'categories' => $categories->get(),
-            'documents'  => $documents->get(),
-        ]);
     }
 }
