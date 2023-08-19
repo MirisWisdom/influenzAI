c3adb228949a968855f5f6664648b1179e907d34
Trung Kien Pham <phmtrungkinn@gmail.com>
2023-08-19 18:39:26 +1000

    Interface Developed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,49 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="mt-6">
-            <a href="/" class="text-center">
-                <h1>Project 2114</h1>
-                <h5><i>By yours truly, G2</i></h5>
-            </a>
+    <header class="title d-none">
+        <a href="/" class="text-center">
+            <h2>InfluenzeAI</h2>
+            <h6><i>By yours truly, G2</i></h6>
+        </a>
+    </header>
+    <div class="container w-75">
+        <div class="card">
+            <div class="card-header text-center">
+                SUBMITTED QUERY
+            </div>
+            <div class="card-body">
+                {{ $query }}
+            </div>
         </div>
-        <form action="{{ route('query') }}"
-              method="post">
-            @csrf
-            <div class="row">
-                <div class="col-md">
-                    <label class="w-100">
-                        Query:
-                        <input type="text"
-                               name="query"
-                               class="form-control w-100">
-                    </label>
-                    <label class="w-100">
-                        <input type="checkbox" name="demo">
-                        Use demo page (super fast but query is ignored!)
-                    </label>
-                </div>
-                <div class="col-md-auto">
-                    <label>
-                        &nbsp;
-                        <input type="submit"
-                               class="form-control"
-                               value="Query">
-                    </label>
-                </div>
-            </div>
-        </form>
         @if(isset($sources))
-            <hr>
-            <div class="card">
-                <div class="card-header text-center">
-                    SUBMITTED QUERY
-                </div>
-                <div class="card-body">
-                    {{ $query }}
-                </div>
-            </div>
             <hr>
             <div class="card">
                 <div class="card-header text-center">
@@ -59,7 +32,7 @@
                     SOURCE DATASETS
                 </div>
                 <div class="card-body">
-                    <div class="row">
+                    <div class="mt-1 row">
                         @foreach($sources as $source)
                             <div class="col-md-6">
                                 <div class="card my-2">
@@ -76,5 +49,30 @@
                 </div>
             </div>
         @endif
+        
+        <div class="position-fixed" style="bottom: 40px; margin: auto; width: 72%;">
+            <hr>
+            <form action="{{ route('query') }}"
+                  method="post">
+                @csrf
+                <div class="row">
+                    <div class="col-md">
+                        <input type="text"
+                        name="query"
+                        class="form-control w-100 p-3 rounded-4 mt-2 ">
+                    </div>
+                    <div class="col-md-auto pl-4 pr-2">
+                        <button type="submit"
+                        name="query"
+                        class="btn btn-secondary form-control p-3 rounded-4 mt-2">
+                        <i class="icon bi bi-send-fill"></i></button>
+                    </div>
+                </div>
+                <label class="w-100">
+                    <input type="checkbox" name="demo">
+                    Use demo page (super fast but query is ignored!)
+                </label>
+            </form>
+        </div>
     </div>
 @endsection
